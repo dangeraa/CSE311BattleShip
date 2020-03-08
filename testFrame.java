af852a46701db2cfe88f2f650b0862bfdebee86b
Andrew Grimes <49619879+DrewGrimes20@users.noreply.github.com>
2020-03-08 15:04:12 -0400

    Add files via upload

---
+++ testFrame.java
@@ -0,0 +1,50 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+
+public class testFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					testFrame frame = new testFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public testFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnThisIsA = new JButton("This is a test Button");
+		btnThisIsA.setBounds(69, 108, 150, 21);
+		contentPane.add(btnThisIsA);
+		
+		JTextArea txtrHelloWorld = new JTextArea();
+		txtrHelloWorld.setText("Hello world");
+		txtrHelloWorld.setBounds(286, 106, 115, 105);
+		contentPane.add(txtrHelloWorld);
+	}
+}
