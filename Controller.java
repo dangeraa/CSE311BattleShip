41061371d0367b5704d30a8d30b68259b43b20ba
dangeraa <48844729+dangeraa@users.noreply.github.com>
2020-03-13 13:12:12 -0400

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,60 +1,60 @@
+/**
+ * Links the user and the system.
+ * Controller takes in user input, passes it into the model and on to one of the views.
+ * @author Abby Danger, Scott Cogen, Andrew Grimes
+ *
+ */
+
 import java.awt.Frame;
 import java.util.Scanner;
-
 import javax.swing.JFrame;
 
 public class Controller {
 
 	public static void main(String[] args) {
 		
+		/*User input decides if the GUI or text-based view will be used*/
 		Scanner input = new Scanner(System.in);
-		
-		
-		
 		int choice = 0;
 		while(choice <1 || choice >2) {
 			System.out.println("(1) for GUI. (2) for Text based");
 			choice = input.nextInt();
 		}
 		
+		/*User chose 1, GUI is called*/
 		if(choice == 1) {
 			ConnectFourFrame cff = new ConnectFourFrame();
-			
 			JFrame jf = new JFrame();
-			
 			jf.setTitle("ConnectFour");
 			jf.setSize(700,700);
 			jf.setVisible(true);
 			jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			
 			jf.getContentPane().add(cff);	
 		}
 		
-		
+		/*User chose 2, text-based is called*/
 		if(choice == 2) {
 			Board testBoard = new Board();
-			
 			Scanner input1 = new Scanner(System.in);
 			int choice1 = 0;
 			char playerTurn = 'r';
-			//While the user has not chosen a vaild choice, it will ask them to make a move.
+			
+			//If user makes invalid choice, it will ask them to make a move
 			while(testBoard.isWinningBoard() == false) {
 				System.out.println(testBoard);	
 				choice = 0;
 				System.out.println("Make a move: choose a column between 1 and 7");
 				choice = input.nextInt();
 				while(choice >= 1 && choice <= 7) {
-					
 					if(testBoard.getMovesMade()%2==0) {
 						playerTurn = 'R';
 					}else {
 						playerTurn = 'Y';
 					}
-					
 					testBoard.makeMove(playerTurn, choice - 1);
 					choice = 0;
 				}
-		}
+			}
 			System.out.println(testBoard);
 			if(testBoard.getMovesMade() % 2 == 0) {
 				System.out.println("Player " + 2 + " Wins!");
